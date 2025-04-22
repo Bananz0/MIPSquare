391e54ba2a01edce97a48aaee6822c6742d5c8e7
Glen Muthoka <glenmuthokaq@gmail.com>
2025-04-22 13:37:58 +0100

    e

--- src/Instruction.h
+++ src/Instruction.h
@@ -14,26 +14,26 @@
 
 namespace InstructionSet {
     //R Type Instructions Definitoins
-    constexpr uint8_t ADD  = 0x20; //0b100000
-    constexpr uint8_t SUB  = 0x22; //0b100010
-    constexpr uint8_t AND  = 0x24; //0b100100
-    constexpr uint8_t OR   = 0x25; //0b100101
-    constexpr uint8_t SLT  = 0x2A; //0b101010
-    constexpr uint8_t SLL  = 0x00; //0b000000
-    constexpr uint8_t SRL  = 0x02; //0b000010
-    constexpr uint8_t SRA  = 0x03; //0b000011
-    constexpr uint8_t JR   = 0x08; //0b001000
+    constexpr uint8_t ADD = 0x20; //0b100000
+    constexpr uint8_t SUB = 0x22; //0b100010
+    constexpr uint8_t AND = 0x24; //0b100100
+    constexpr uint8_t OR = 0x25; //0b100101
+    constexpr uint8_t SLT = 0x2A; //0b101010
+    constexpr uint8_t SLL = 0x00; //0b000000
+    constexpr uint8_t SRL = 0x02; //0b000010
+    constexpr uint8_t SRA = 0x03; //0b000011
+    constexpr uint8_t JR = 0x08; //0b001000
 
     //I Type Instructions Definitions
     constexpr uint8_t ADDI = 0x08; //0b001000
-    constexpr uint8_t LW   = 0x23; //0b100011
-    constexpr uint8_t SW   = 0x2B; //0b101011
-    constexpr uint8_t BEQ  = 0x04; //0b000100
-    constexpr uint8_t BNE  = 0x05; //0b000101
+    constexpr uint8_t LW = 0x23; //0b100011
+    constexpr uint8_t SW = 0x2B; //0b101011
+    constexpr uint8_t BEQ = 0x04; //0b000100
+    constexpr uint8_t BNE = 0x05; //0b000101
 
     //J Type Instruction Definitions
-    constexpr uint8_t J    = 0x02; //0b000010
-    constexpr uint8_t JAL  = 0x03; //0b000011
+    constexpr uint8_t J = 0x02; //0b000010
+    constexpr uint8_t JAL = 0x03; //0b000011
 }
 
 enum class InstructionType {
@@ -54,11 +54,13 @@ public:
     [[nodiscard]] uint16_t getImmediate() const;
     [[nodiscard]] uint32_t getJumpTarget() const;
     [[nodiscard]] uint8_t getFunct() const;
+
     [[nodiscard]] std::string toString() const;
 
     void parseRawInstruction(uint32_t raw);
 
-    static void parseAssemblyInstruction(const std::string& asm_text);
+    static void parseAssemblyInstruction(const std::string &asm_text);
+
 private:
     InstructionType type = InstructionType::Uninitialized;
     uint8_t opcode = 0;
